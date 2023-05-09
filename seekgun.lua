0e08fdd85279da6bac6e79fe207e6d23c080f4c2
poiu <75510171+notpoiu@users.noreply.github.com>
2023-05-09 15:40:45 -0400

    Update seekgun.lua

--- seekgun.lua
+++ seekgun.lua
@@ -1,6 +1,4 @@
 -- Made by upio for legit doors
--- ty oogway for the gun idle animation id
-
 local Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
 local CustomShop = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Shop%20Items/Source.lua"))()
 
@@ -9,21 +7,13 @@ local mainui = player.PlayerGui.MainUI
 local mouse = player:GetMouse()
 
 local seekGun = game:GetObjects("rbxassetid://13386404155")[1] or LoadCustomInstance("https://raw.githubusercontent.com/persopoiu/scripts/main/SeekGunAssets/SeekPistol.rbxm")
-local seekGoo = game:GetObjects("rbxassetid://13386436753")[1] or LoadCustomInstance("https://raw.githubusercontent.com/persopoiu/scripts/main/SeekGunAssets/SeekAmmo.rbxm")
-
-seekGun.TextureId = LoadCustomAsset("https://raw.githubusercontent.com/persopoiu/scripts/main/SeekGunAssets/SeekShopDisplay.png")
-
-local anim = Instance.new("Animation")
-anim.Name = "M249Idle"
-anim.AnimationId = "rbxassetid://3034291703"
-
-local track = game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(anim)
+local seekAmmo = game:GetObjects("rbxassetid://13386436753")[1] or LoadCustomInstance("https://raw.githubusercontent.com/persopoiu/scripts/main/SeekGunAssets/SeekAmmo.rbxm")
 
 if mainui.ItemShop and mainui.ItemShop.Visible then
     CustomShop.CreateItem(seekGun, {
         Title = _G.ShopName or "Seek Gun",
         Desc = _G.ShopDescription or "Seekifies Anything that it shoots",
-        Image = "https://raw.githubusercontent.com/persopoiu/scripts/main/SeekGunAssets/SeekShopDisplay.png",
+        Image = "rbxassetid://13390436314",
         Price = _G.ShopPrice or 300,
         Stack = 1,
     })
@@ -41,33 +31,30 @@ function playSound(rbxassetid,part)
     end)
 end
 
-seekGun.Equipped:Connect(function()
-    track:Play()
-end)
-
-seekGun.Unequipped:Connect(function()
-    track:Stop()
-end)
 
 seekGun.Activated:Connect(function()
-    local seekGooClone = seekGoo:Clone()
-    local velocity = mouse.Hit.LookVector * 0.5 * (_G.launchForce * 10)
+    local seekAmmoClone = seekAmmo:Clone()
     local spawnPos = workspace.CurrentCamera.CFrame:ToWorldSpace(CFrame.new(0,0,-3) * CFrame.lookAt(Vector3.new(0, 0, 0), workspace.CurrentCamera.CFrame.LookVector))
     
-    seekGooClone.Parent = workspace
-    seekGooClone.CFrame = spawnPos
-    seekGooClone.Velocity = velocity
+    local Attachment = Instance.new("Attachment", seekAmmoClone)
+    local LV = Instance.new("LinearVelocity", Attachment)
+    LV.MaxForce = math.huge
+    LV.VectorVelocity = mouse.Hit.LookVector * 0.5 * ((_G.launchForce or 20) * 10)
+    LV.Attachment0 = Attachment
+
+    seekAmmoClone.Parent = workspace
+    seekAmmoClone.CFrame = spawnPos
 
     playSound("rbxassetid://1905367471")
 
-    seekGooClone.Touched:Connect(function(hit)
-        seekGooClone:Destroy()
-        playSound("https://raw.githubusercontent.com/persopoiu/scripts/main/SeekGunAssets/landedsfx.mp3")
+    seekAmmoClone.Touched:Connect(function(hit)
+        seekAmmoClone:Destroy()
+        playSound("rbxassetid://344167846")
         
         task.spawn(function()
             local model = hit:FindFirstAncestorWhichIsA("Model")
 
-            if model ~= nil and model.Parent ~= workspace.CurrentRooms or model.Name ~= "Assets" then
+            if model ~= nil and not (workspace.CurrentRooms:FindFirstChild(model.Name)) then
                 local seekpointlight = Instance.new("PointLight",model)
                 seekpointlight.Range = 10
                 seekpointlight.Brightness = 3
@@ -76,8 +63,12 @@ seekGun.Activated:Connect(function()
                 for _,instance in ipairs(model:GetDescendants()) do
                     if instance:IsA("BasePart") or instance:IsA("Part") or instance:IsA("MeshPart") then
                         instance.Anchored = false
-                        instance.Material = Enum.Material.Foil
+                        instance.Material = "Foil"
                         instance.Color = Color3.fromRGB(0,0,0)
+                    end
+
+                    if instance:IsA("TextLabel") then
+                        instance.TextColor3 = Color3.fromRGB(255,255,255)
                     end
                 end
 
